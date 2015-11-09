59779e6ecdfd85a9edeb709cc8bb377dcaaafa32
umicorn <polibo.felix@gmail.com>
2015-11-09 13:37:46 -0600

    got progam to start

--- app/src/main/java/com/example/jchuah/mazegame/Q2.java
+++ app/src/main/java/com/example/jchuah/mazegame/Q2.java
@@ -1,43 +1,22 @@
 package com.example.jchuah.mazegame;
 
-import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-
-    Bundle groceries;
-
-    public void onColorClick(View source) {
-        Intent Q1 = new Intent(this, q1.class);
-        Q1.putExtra("groceries", groceries);
-
-        Button colorButton = (Button) source;
-        ColorDrawable bgColor = (ColorDrawable) source.getBackground();
-
-        groceries.putInt("colorclick", bgColor.getColor());
-
-        startActivity(Q1);
-    }
+public class Q2 extends AppCompatActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i("chuah debug", "activity launch");
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_q2);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_q2, menu);
         return true;
     }
 
@@ -55,5 +34,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
 }
